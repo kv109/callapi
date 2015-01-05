90019a0c0440cf78707c0648acc27992c49892fe
kv109 <kacper@walanus.com>
2015-01-05 13:40:23 +0100

    namespaces in routes

--- lib/callapi/routes.rb
+++ lib/callapi/routes.rb
@@ -11,10 +11,30 @@ class Callapi::Routes
     def get(*args)
       call_name = args.shift
       camelized_call_name = call_name.camelize
-      full_class_name = camelized_call_name
-      full_class_name.split('::').inject(Get) do |k, class_name|
+      camelized_call_name_with_namespaces = namespaces + [camelized_call_name]
+      camelized_call_name_with_namespaces.inject(Get) do |k, class_name|
         k.const_set(class_name, Class.new(Callapi::Call::Base))
       end
+    end
+
+    def namespace(*args)
+      add_namespace(args.shift)
+      yield
+      remove_namespace
+    end
+
+    private
+
+    def namespaces
+      @namespaces ||= []
+    end
+
+    def add_namespace(namespace)
+      namespaces << namespace.to_s.camelize
+    end
+
+    def remove_namespace
+      namespaces.pop
     end
 
     def build_http_method_namespaces
--- spec/unit/routes_spec.rb
+++ spec/unit/routes_spec.rb
@@ -16,6 +16,25 @@ describe Callapi::Routes do
         expect( Get::Version.superclass ).to eql(Callapi::Call::Base)
       end
     end
+  end
 
+  context '#namespace' do
+    let(:args) { ['version'] }
+
+    before do
+      arguments = args.dup
+      described_class.draw do
+        namespace :users do
+          namespace :posts do
+            get *(arguments)
+          end
+        end
+      end
+    end
+
+    it 'should create namespace' do
+      expect{ Get::Users::Posts }.to_not raise_error{ NameError }
+      expect( Get::Users::Posts.superclass ).to eql(Callapi::Call::Base)
+    end
   end
 end
