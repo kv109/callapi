fc5c877f88a3d4fd6194c583c4189278150e226b
Kacper Walanus <kacper.walanus@elpassion.pl>
2015-01-10 12:35:55 +0100

    spec added

--- spec/unit/routes_spec.rb
+++ spec/unit/routes_spec.rb
@@ -2,39 +2,48 @@ require 'spec_helper'
 
 describe Callapi::Routes do
   context '#get' do
-    let(:args) { ['version'] }
+    context 'should create new class' do
 
-    before do
-      arguments = args.dup
-      described_class.draw do
-        get *(arguments)
+      before do
+        described_class.draw do
+          get 'version'
+        end
+      end
+
+      it 'which inherits from Call::Base' do
+        expect( Callapi::Get::Version.superclass ).to eql(Callapi::Call::Base)
       end
     end
 
-    context 'should create new class' do
-      it 'which inherits from Call::Base' do
-        expect( Callapi::Get::Version.superclass ).to eql(Callapi::Call::Base)
+    context 'options' do
+      context ':strategy' do
+
+        before do
+          described_class.draw do
+            get 'users', strategy: Callapi::Call::Request::Mock
+          end
+        end
+
+        it 'should set request strategy for created call' do
+          expect( Callapi::Get::Users.strategy ).to eql(Callapi::Call::Request::Mock)
+        end
       end
     end
   end
 
   context '#namespace' do
-    let(:args) { ['version'] }
-
     before do
-      arguments = args.dup
       described_class.draw do
         namespace :users do
           namespace :posts do
-            get *(arguments)
+            get 'index'
           end
         end
       end
     end
 
-    it 'should create namespace' do
-      expect{ Callapi::Get::Users::Posts }.to_not raise_error{ NameError }
-      expect( Callapi::Get::Users::Posts.superclass ).to eql(Callapi::Call::Base)
+    it 'should surround created call with namespace' do
+      expect( Callapi::Get::Users::Posts::Index.superclass ).to eql(Callapi::Call::Base)
     end
   end
 end
