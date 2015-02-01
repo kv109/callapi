faca582ba68cd0326bedb17c26af3550639e5ecd
Kacper Walanus <kacper.walanus@elpassion.pl>
2015-02-01 21:11:31 +0100

    json_spec refactored

--- spec/unit/call/response/json_spec.rb
+++ spec/unit/call/response/json_spec.rb
@@ -5,7 +5,6 @@ describe Callapi::Call::Response::Json do
     before do
       config = Callapi::Config
       allow(config).to receive(:api_host).and_return('http://api.org')
-      stub_request(:get, 'http://api.org/users').to_return(status: 200, body: '{"json": true}')
 
       call_class = Get::Users
       @call = call_class.new
@@ -13,8 +12,10 @@ describe Callapi::Call::Response::Json do
 
     subject { @call.response.data }
 
-    it 'should return object' do
-      expect(subject['json']).to eql ( true )
+    context 'when request body is "{"valid": true, "score": 59}"' do
+      before { stub_request(:get, 'http://api.org/users').to_return(status: 200, body: '{"valid": true, "score": 59}') }
+
+      it { expect(subject).to eq ({'valid' => true, 'score' => 59}) }
     end
   end
 end
