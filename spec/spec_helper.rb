40f58fce60ba3ebf2843001532a91e74fe40ec63
Kacper Walanus <kacper.walanus@elpassion.pl>
2015-02-15 16:35:56 +0100

    do not display logs during spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
     Get::Users = Class.new(Callapi::Call::Base)
 
     Callapi::Config.configure do |config|
-      config.log_level = :off
+      config.log_level = :none
     end
   end
 end
